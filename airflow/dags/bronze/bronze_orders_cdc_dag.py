01b4f394aff8516ce10abc316abbfddec46ada65
Caio Henrique Andrade Lima <caiohandradelima@Caios-MacBook-Air.local>
2026-01-26 17:36:44 -0300

    fix ci using black formatter

--- airflow/dags/bronze/bronze_orders_cdc_dag.py
+++ airflow/dags/bronze/bronze_orders_cdc_dag.py
@@ -1,18 +1,18 @@
 from datetime import datetime
 
+from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 
 from airflow import DAG
 from src.ingestion.postgres.cdc.orders_cdc_consumer import consume_and_write_orders
-from airflow.operators.empty import EmptyOperator
 
 with DAG(
-        dag_id="bronze_kafka_order_events",
-        start_date=datetime(2024, 1, 1),
-        schedule_interval="@hourly",
-        catchup=False,
-        max_active_runs=1,
-        tags=["bronze", "kafka", "cdc"],
+    dag_id="bronze_kafka_order_events",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="@hourly",
+    catchup=False,
+    max_active_runs=1,
+    tags=["bronze", "kafka", "cdc"],
 ) as dag:
     start = EmptyOperator(task_id="start")
     end = EmptyOperator(task_id="end")
