afc8c82d9dc4004eaf32a05240c2107816c280ec
Caio Henrique Andrade Lima <caiohandradelima@Caios-MacBook-Air.local>
2026-02-02 10:59:44 -0300

    leave a fixed version of airbyte

--- infra/local/airbyte/start_airbyte.sh
+++ infra/local/airbyte/start_airbyte.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 abctl local install \
+  --chart-version 2.0.19 \
   --host airbyte.local \
-  --insecure-cookies
+  --insecure-cookies \
+  --values airbyte-values.yaml \
+  --port=8000
